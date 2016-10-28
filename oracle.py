4fa5c9642af2ea167b844a3c56c09a534ce696c4
Frederick Robson <frederickwlrobson@gmail.com>
2016-10-27 22:48:48 -0700

    almost done

--- oracle.py
+++ oracle.py
@@ -1,32 +1,31 @@
-import csv,sys
+import csv,sys, Queue 
 import sqlite3 as lite 
+import  databaseUtil
 
 TERMS = [36,60] #Possible lengths of loans
 DATA_FILE = 'data/loan.csv'
 
-def set_up_db():
-	try:
-	    con = lite.connect('data/database.sqlite')    
-	    cur = con.cursor()
-	    return cur
-    
-	except lite.Error, e:	    
-	    print "Error %s:" % e.args[0]
-	    sys.exit(1)
-	    
-	finally:
-	    if con:
-	        con.close()
 
-def choose_best_portfolio(intitial_investment, issue_date, term):
-	'''
-	Chooses the optimal portfolio of loans for a chosen issue date and term
-	@params: 
-		initial_investment: the $value of the initial investment (float)
-		issue_date: the issue date of the loans (string)
-		term
 
-	'''
-	pass
+class oracle():
+	db = databaseUtil.databaseAccess()
 
-set_up_db()
+	@staticmethod 
+	def choose_best_portfolio(intitial_investment, month, term):
+		'''
+		Chooses the optimal portfolio of loans for a chosen issue date and term
+		@params: 
+			initial_investment: the $value of the initial investment (float)
+			issue_date: the issue date of the loans (string) eg "Mar-2011"
+			term
+		'''
+		loans = oracle.db.get_loans_issued_in(month)
+		pq = Queue.PriorityQueue()
+		for l in loans:
+			total_payment = l[39]
+			funded_amnt = l[4]
+			percent_return_td  = total_payment/funded_amnt
+			pq.put(percent_return_td,loan)
+
+oracle.choose_best_portfolio(1,"Mar-2014",36)
+
